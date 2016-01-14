afef34cb0242ed8d9873b5ee564ca0e948a915b7
Andrey Belyaev <svetomech@gmail.com>
2016-01-14 16:09:55 +0300

    Rewrote Application.cs using C# 6.0
    Almost 3x size reduction. Hope I didn't mess anything up.

--- SimpleMaid/Application.cs
+++ SimpleMaid/Application.cs
@@ -8,59 +8,16 @@ namespace SimpleMaid
 {
   public class Application
   {
-    private Assembly assembly;
-    private FileVersionInfo assemblyInfo;
+    private static readonly Assembly assembly = Assembly.GetEntryAssembly();
+    private static readonly FileVersionInfo assemblyInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
 
-
-    private string companyName;
-    private string productName;
-    private string productVersion;
-    private string executablePath;
-    private string directory;
-    private string guid;
-    private bool hidden;
-
-    private string state;
-
-
-    public string CompanyName { get { return companyName; } }
-    public string ProductName { get { return productName; } }
-    public string ProductVersion { get { return productVersion; } }
-    public string ExecutablePath { get { return executablePath; } }
-    public string Directory { get { return directory; } }
-    public string Guid { get { return guid; } }
-    public bool Hidden { get { return hidden; } set { hidden = value; } }
-
-    /// <summary>
-    /// Supports: "idle", "busy", "fail"
-    /// </summary>
-    public string State
-    {
-      get { return state; }
-      set
-      {
-        state = value;
-        Console.Title = String.Format("{0}: {1}", productName, state);
-      }
-    }
-
-
-    public Application()
-    {
-      assembly = Assembly.GetEntryAssembly();
-      assemblyInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-
-
-      companyName = assemblyInfo.CompanyName;
-      productName = assemblyInfo.ProductName;
-      productVersion = assemblyInfo.ProductVersion;
-      executablePath = assembly.Location;
-      directory = Path.GetDirectoryName(executablePath) + "\\";
-      guid = ((GuidAttribute)assembly.GetCustomAttributes(
-        typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
-      hidden = false;
-
-      state = null;
-    }
+    public string CompanyName => assemblyInfo.CompanyName;
+    public string ProductName => assemblyInfo.ProductName;
+    public string ProductVersion => assemblyInfo.ProductVersion;
+    public string ExecutablePath => assembly.Location;
+    public string Directory => Path.GetDirectoryName(assembly.Location) + "\\";
+    public string Guid => ((GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value;
+    public bool Hidden { get; set; } = false;
+    public string State { get { return State; } set { Console.Title = $"{ProductName}: {State}"; } }
   }
 }
