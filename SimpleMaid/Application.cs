d77c9c5b1b49db04181e4cb07ae591bb71b7849b
Andrey Belyaev <svetomech@gmail.com>
2016-01-14 17:36:31 +0300

    Removed workaround with trailing "\" at the end
    I have to now implement proper use of DirectoryInfo/FileInfo

--- SimpleMaid/Application.cs
+++ SimpleMaid/Application.cs
@@ -15,7 +15,7 @@ namespace SimpleMaid
     public string ProductName => assemblyInfo.ProductName;
     public string ProductVersion => assemblyInfo.ProductVersion;
     public string ExecutablePath => assembly.Location;
-    public string Directory => Path.GetDirectoryName(assembly.Location) + "\\";
+    public string Directory => Path.GetDirectoryName(assembly.Location);
     public string Guid => ((GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value;
     public bool Hidden { get; set; } = false;
     public string State { get { return State; } set { Console.Title = $"{ProductName}: {State}"; } }
