2b3c2b59e6989d7d200de71b914dd7d73a5ee669
Andrey Belyaev <svetomech@gmail.com>
2016-04-05 22:00:05 +0300

    Added small comment about UnsecurePasswordPrompt

--- SimpleMaid/Svetomech.Utilities/SimpleConsole.cs
+++ SimpleMaid/Svetomech.Utilities/SimpleConsole.cs
@@ -56,7 +56,7 @@ namespace Svetomech.Utilities
     }
 
     /* HACK (to get the actual value, not an object): new NetworkCredential(String.Empty, PasswordPrompt("Enter a password: ")).Password;
-         N.B.! It's really dirty, kills the purpose of using a SecureString and doesn't even work in Mono */
+         N.B.! It's really dirty, kills the purpose of using a SecureString and doesn't even work in Mono. Better use Unsecure method. */
     public static SecureString PasswordPrompt(string hintMessage)
     {
       var pass = new SecureString();
